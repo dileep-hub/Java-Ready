893448a29f08540613e8d4aacf63e0c4804938fb
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2021-09-01 08:07:49 +0530

    Create TwoSum.java

---
+++ 5.Array_ArrayList/TwoSum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int[] res = new int[2];
+        
+        for (int i=0; i<nums.length; i++)
+        {
+            for(int j=0; j<nums.length; j++)
+            {
+                if (j!=i)
+                {
+                    if (nums[i]+nums[j] == target)
+                    {
+                        res[0] = i;
+                        res[1] = j;
+                        return res;
+                    }
+                }
+            }
+        }
+        return res;
+        
+    }
+}
