fb8cd264a898e3f28a13b8ba9acc08625abc0121
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2021-10-22 08:57:03 +0530

    Create P-toggleString.java

---
+++ 08.Strings/Problems/P-toggleString.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static String toggleCase(String str){
+		//write your code here
+	
+	StringBuilder sb = new StringBuilder(str);
+	
+	for(int i=0; i<sb.length(); i++) {
+	    char ch = sb.charAt(i);
+	    
+	    // if ch is lowercase 
+	    if(ch >= 'a' && ch <= 'z') {
+	        char uch = (char)('A' + ch - 'a');
+	        sb.setCharAt(i, uch);
+	    } else if(ch >= 'A' && ch <= 'Z') { // if ch is uppercase
+	        char lch = (char)(-'A' + ch + 'a');
+	        sb.setCharAt(i, lch);
+	    }
+	}
+	
+	return sb.toString();
+	
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(toggleCase(str));
+	}
+
+}
+
+// Sample Input
+// pepCODinG
+// Sample Output
+// PEPcodINg
