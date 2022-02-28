3089b543669f4e7f2c6990a8e265a064cb46c425
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2022-02-28 12:41:29 +0530

    Add files via upload

---
+++ 18.LinkedList/Problems/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package com.dileep.Problems;
+
+public class RemoveDuplicates  {
+
+    // remove duplicates
+    public void duplicates() {
+//        Node node = head;
+//
+//        while (node.next != null) {
+//            if(node.value == node.next.value) {
+//                node.next = node.next.next;
+//                size--;
+//            } else  {
+//                node = node.next;
+//            }
+//        }
+//        tail = node;
+//        tail.next = null;
+    }
+
+
+}
